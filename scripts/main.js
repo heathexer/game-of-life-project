64b3e0f42c952bafdca9d9f8b83e24449a3abef3
heathexer <heathexer@gmail.com>
2016-08-23 21:24:27 -0700

    Cells can now check how many live neighbors they have

--- scripts/main.js
+++ scripts/main.js
@@ -1,20 +1,25 @@
 var c = document.getElementById("mainCanvas");
 var canvas = c.getContext('2d');
+document.getElementById("start").onclick = start;
 
+var runSpeed = 1;
 var canvasWidth = c.width;
 var canvasHeight = c.height;
-canvas.lineWidth = 1;
+canvas.lineWidth = 0.5;
 canvas.globalAlpha = 1.0;
 canvas.imageSmoothingEnable = false;
-var pixelsWide = 25;
+var pixelsWide = 10;
 var pixels = new Array(pixelsWide);
 var pixelWidth = canvasWidth/pixelsWide;
 var running = false;
 
-function Pixel(posX, posY) {
+function Pixel(posX, posY, row, column) {
     this.x = posX;
     this.y = posY;
+    this.row = row;
+    this.column = column;
     this.alive = false;
+    this.numNeighbors = 0;
     this.draw = function() {
         if(this.alive) {
             canvas.fillStyle = "black";
@@ -23,7 +28,7 @@ function Pixel(posX, posY) {
         }
         canvas.fillRect(this.x, this.y, pixelWidth, pixelWidth);
         drawLines();
-    }
+    };
     this.toggle = function() {
         if(this.alive) {
             this.alive = false;
@@ -31,13 +36,67 @@ function Pixel(posX, posY) {
             this.alive = true;
         }
         this.draw();
+    };
+
+    this.checkNeighbors = function() {
+        this.numNeighbors = 0;
+        //North
+        if(this.row > 0) {
+            if (pixels[this.row - 1][this.column].alive) {
+                this.numNeighbors++;
+            }
+        }
+        //North East
+        if(this.row > 0 && this.column < pixelsWide-1) {
+            if (pixels[this.row - 1][this.column + 1].alive) {
+                this.numNeighbors++;
+            }
+        }
+        //East
+        if(this.column < pixelsWide-1) {
+            if (pixels[this.row][this.column + 1].alive) {
+                this.numNeighbors++;
+            }
+        }
+        //South East
+        if(this.column < pixelsWide-1 && this.row < pixelsWide-1) {
+            if (pixels[this.row + 1][this.column + 1].alive) {
+                this.numNeighbors++;
+            }
+        }
+        //South
+        if(this.row < pixelsWide-1) {
+            if (pixels[this.row + 1][this.column].alive) {
+                this.numNeighbors++;
+            }
+        }
+        //South West
+        if(this.row < pixelsWide-1 && this.column > 0) {
+            if (pixels[this.row + 1][this.column - 1].alive) {
+                this.numNeighbors++;
+            }
+        }
+        //West
+        if(this.column > 0) {
+            if (pixels[this.row][this.column - 1].alive) {
+                this.numNeighbors++;
+            }
+        }
+        //North West
+        if(this.column > 0 && this.row > 0) {
+            if (pixels[this.row - 1][this.column - 1].alive) {
+                this.numNeighbors++;
+            }
+        }
+
+        return this.numNeighbors;
     }
-};
+}
 function generatePixels() {
     for(var i = 0; i<pixelsWide; i++) {
         pixels[i] = new Array(pixelsWide);
         for(var j = 0; j<pixelsWide; j++) {
-            pixels[i][j] =  new Pixel(i*(pixelWidth), j*(pixelWidth));
+            pixels[i][j] =  new Pixel(i*(pixelWidth), j*(pixelWidth), i, j);
             pixels[i][j].draw();
         }
     }
@@ -57,9 +116,17 @@ function drawLines() {
         canvas.stroke();
     }
 }
+function start() {
+    if(running) {
+        running = false;
+    } else {
+        running = true;
+    }
+}
+function run() {
+    if(running) {
 
-while(running) {
-    
+    }
 }
 
 function getMousePos() {
@@ -76,4 +143,5 @@ function click() {
 }
 generatePixels();
 drawLines();
+setInterval(run(), 1000/runSpeed);
 c.addEventListener("click", click);
